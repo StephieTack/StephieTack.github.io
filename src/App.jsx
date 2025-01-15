588161c166a6905c1eb96b8b31884bc77723763a
Stephanie Tack <stephierathjen@gmx.de>
2025-01-15 13:58:36 +0100

    initialize portfolio files to this repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Skill from "./components/Skill";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+      </main>
+    </>
+  );
+}
